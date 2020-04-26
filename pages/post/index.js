2d81d5140be09c9838bbc5daa1ac0a00fe7870ca
Kage Soichiro <k4gepedia@gmail.com>
2020-04-26 15:59:36 +0900

    next contentful

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,12 +1,48 @@
+import { useEffect, useState } from 'react'
 import Head from '../../components/head';
 import Nav from '../../components/nav';
+import Post from '../../components/post'
 
-export default () => (
+const client = require('contentful').createClient({
+  space: process.env.CTF_SPACE_ID,
+  accessToken: process.env.CTF_CDA_ACCESS_TOKEN
+})
+
+function HomePage() {
+  async function fetchEntries() {
+    const entries = await client.getEntries()
+    if (entries.items) return entries.items
+    console.log(`Error getting Entries for ${contentType.name}.`)
+  }
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    async function getPosts() {
+      const allPosts = await fetchEntries()
+      setPosts([...allPosts])
+    }
+    getPosts()
+  }, [])
+}
+
+export default () => (  
   <div>
     <Head title="Home" />
     <Nav />
     <div className="hero">
       <h1 className="title">Welcome to Post Pages</h1>
+      {posts.length > 0
+        ? posts.map(p => (
+            <Post
+              title={p.fields.title}
+              publishDate={p.fields.publishDate}
+              key={p.fields.title}
+              url={p.fields.url}
+              slug={p.fields.slug}
+            />
+          ))
+        : null}
     </div>
 
     <style jsx>{`
