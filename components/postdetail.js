ab6cd498e7f2f552cbd405bfdbe5fc636a9123f7
Kage Soichiro <k4gepedia@gmail.com>
2020-05-01 00:26:01 +0900

    markdown-it

--- components/postdetail.js
+++ components/postdetail.js
@@ -1,10 +1,17 @@
+const md = require('markdown-it')({
+  html: true,
+  lineNumbers: true
+})
+.use(require('markdown-it-highlightjs'))
+.use(require('markdown-it-katex'))
+
 const Post = ({ title, publishDate, discription, body }) => (
     <div className="container">
         <div className="text">
           <h2>{title}</h2>
           <h4>{publishDate}</h4>
           <p>{discription}</p><br />
-          <p>{body}</p>
+          <div dangerouslySetInnerHTML={{__html: md.render(body)}} />
         </div>
 
     <style jsx>{`
