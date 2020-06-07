62171bb394c973a7773f159e3137ba34a9317a29
Kage Soichiro <k4gepedia@gmail.com>
2020-06-07 17:45:51 +0900

    markdown-it add plugin

--- components/postdetail.js
+++ components/postdetail.js
@@ -1,11 +1,25 @@
+// markdown-it-plugin
 const md = require('markdown-it')({
   breaks: true,
   html: true,
   linkify: true,
   typography: true,
 })
+.use(require('markdown-it-emoji'))
+.use(require('markdown-it-sup'))
+.use(require('markdown-it-sub'))
+.use(require('markdown-it-ins'))
+.use(require('markdown-it-mark'))
+.use(require('markdown-it-footnote'))
+.use(require('markdown-it-deflist'))
+.use(require('markdown-it-abbr'))
+.use(require('markdown-it-container'), "info")
+.use(require('markdown-it-container'), "success")
+.use(require('markdown-it-container'), "warning")
+.use(require('markdown-it-container'), "danger")
 .use(require('markdown-it-highlightjs'))
 .use(require('markdown-it-katex'))
+// markdown-it-plugin
 
 const Post = ({ title, publishDate, discription, body }) => (
     <div className="container">
