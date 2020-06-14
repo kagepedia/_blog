bbe6f17f526a9cd16c3b0ddfafad06561abc8157
Kage Soichiro <k4gepedia@gmail.com>
2020-06-14 11:42:49 +0900

    stylesheet add

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,13 +1,13 @@
 import Head from '../../components/head';
 import Nav from '../../components/nav';
-import styles from "./contact.module.css"
+import '../../styles/contact/contact.scss'
 
 export default () => (
   <div>
     <Head title="Home" />
     <Nav />
     <div className="hero">
-      <h1 className={styles.title}>Welcome to Conatct Pages</h1>
+      <h1>Welcome to Conatct Pages</h1>
     </div>
   </div>
 );
