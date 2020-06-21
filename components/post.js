77e6d16d0099c7108a5f262a925cdd20254cb304
Kage Soichiro <k4gepedia@gmail.com>
2020-06-22 02:02:45 +0900

    post list styling

--- components/post.js
+++ components/post.js
@@ -1,16 +1,16 @@
 import Link from 'next/link';
 const Post = ({ img_url, img_alt, title, publishDate, discription, slug }) => (
   <Link href="/post/[slug]" as={`/post/${slug}`}>
+    <a>
     <div className="container">
-      <div className="thumbnail">
-        <img src={img_url} alt={img_alt} />
-      </div>
+      <img className="thumbnail" src={img_url} alt={img_alt} />
       <div className="text">
-        <h4 className="date">{publishDate}</h4>
         <h2 className="ttl">{title}</h2>
+        <h4 className="date">{publishDate}</h4>
         <p className="explain">{discription}</p>
       </div>
     </div>
+    </a>
   </Link>
 );
 
